3c0cc458580d9254148bf9544d4f7134af8a331c
viktorv <viktorvelev8@gmail.com>
2017-09-03 20:40:49 +0300

    Main method added.

---
+++ src/CowsAndBullsEngine.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author viktorv
+ *
+ */
+public class CowsAndBullsEngine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
