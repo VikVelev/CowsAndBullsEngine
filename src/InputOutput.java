15830d48eb45897fae6c8ec866b45dcfdd7aff31
viktorv <viktorvelev8@gmail.com>
2017-09-03 22:07:51 +0300

    Added a method for Smart output depending on the input, plural or singular form

---
+++ src/InputOutput.java
@@ -0,0 +1,27 @@
+
+public class InputOutput {
+	
+	public static void Input() {
+		
+	}
+	
+	public static void Output(int cows, int bulls) {
+		
+		String cowStr = "";
+		String bullStr = "";
+		
+		if(cows == 1) {
+			cowStr = "cow";
+		} else {
+			cowStr = "cows";
+		}
+		
+		if(bulls == 1) {
+			bullStr = "bull";
+		} else {
+			bullStr = "bulls";
+		}
+		
+		System.out.println("There are" + cows + " " + cowStr + ", " + bulls + " " + bullStr);	
+	}
+}
